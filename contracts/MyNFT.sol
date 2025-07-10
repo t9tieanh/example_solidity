dd5f70559ccdaa3d9f3d8c81eb52c15c153c4906
t9tieanh <phama9162@gmail.com>
2025-07-10 21:09:04 +0700

    add 6.3 myNFT myToken

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+    uint public nextTokenId;
+    address public admin;
+
+    constructor() ERC721("MyNFT", "MNFT") {
+        admin = msg.sender;
+    }
+
+    function mint(address to) external {
+        require(msg.sender == admin, "only admin");
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+}
